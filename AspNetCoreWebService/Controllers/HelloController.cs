a27b5abfa5975adf5edd1a57f1e8b387682e31fd
HninElena <79574347+HninElena@users.noreply.github.com>
2021-04-04 19:07:11 +0800

    Update HelloController.cs

--- AspNetCoreWebService/Controllers/HelloController.cs
+++ AspNetCoreWebService/Controllers/HelloController.cs
@@ -24,7 +24,7 @@ namespace AspNetCoreWebService.Controllers
         }
         [Fact]
         public void NoInputParamGetResponseTest()
-        {
+        { 
             HelloController controller = new HelloController();
             var response = controller.Get().Value as Response;
             Assert.Equal("Hello World!!!", response.output);
